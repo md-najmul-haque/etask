21d5fe07ff7ccbb0c2fa2fc40a0b6ac09d0d5bc5
najuml-bd <najmul0612@gmail.com>
2022-06-30 15:11:44 +0600

    initial setup with Navbar and Footer added

---
+++ src/Pages/Shared/Footer.js
@@ -0,0 +1,20 @@
+import { BsLinkedin, BsGithub, BsFacebook } from 'react-icons/bs';
+import React from 'react';
+
+
+const Footer = () => {
+    const year = new Date().getFullYear()
+    return (
+
+        <footer className="footer bg-black footer-center text-white py-10">
+            <div className='grid grid-flow-col gap-4 pb-0 mb-0 text-3xl' >
+                <a href="https://www.linkedin.com/in/md-najmul-haque/" rel="noreferrer" role='button' target='_blank'><BsLinkedin /></a>
+                <a href="https://github.com/md-najmul-haque" rel="noreferrer" role='button' target='_blank'><BsGithub /></a>
+                <a href="https://www.facebook.com/KupjolerJahaji" rel="noreferrer" role='button' target='_blank'><BsFacebook /></a>
+            </div>
+            <p>Copyright © {year} - All right reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
